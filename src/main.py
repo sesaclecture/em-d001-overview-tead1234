fad032a56739bd3dbe43d4cfb000543492a3d141
jpko <jpko>
2026-06-05 14:27:55 +0900

    test

--- src/main.py
+++ src/main.py
@@ -4,7 +4,8 @@
 #
 # 강의 시간에 실습한 Linux 명령어를 활용하세요.
 def get_current_path_command():
-    raise NotImplementedError
+    return "pwd"
+    # raise NotImplementedError
 
 
 # 문제 2.
@@ -21,7 +22,11 @@ def get_current_path_command():
 #
 # 강의 시간에 사용한 옵션을 활용하세요.
 def make_list_command(hidden):
-    raise NotImplementedError
+    if hidden:
+        return "ls -al"
+    else:
+        return "ls"
+    # raise NotImplementedError
 
 
 # 문제 3.
@@ -30,7 +35,8 @@ def make_list_command(hidden):
 #
 # path 인자는 이동할 디렉토리 경로입니다.
 def make_change_directory_command(path):
-    raise NotImplementedError
+    return "cd " + path
+    # raise NotImplementedError
 
 
 # 문제 4.
@@ -41,7 +47,8 @@ def make_change_directory_command(path):
 #
 # 강의 중 확인한 U-Boot 명령어를 활용하세요.
 def make_printenv_command(env_name):
-    raise NotImplementedError
+    return f"printenv ${env_name}"
+    # raise NotImplementedError
 
 
 # 문제 5.
@@ -52,11 +59,12 @@ def make_printenv_command(env_name):
 # rootfs 인자는 루트 파일시스템 장치 이름입니다.
 #
 # 생성되는 명령어에는 아래 정보가 포함되어야 합니다.
-#
+#s
 # - console
 # - root
 # - rw
 #
 # 강의 중 실습한 bootargs 형식을 참고하세요.
 def make_bootargs_command(console, rootfs):
-    raise NotImplementedError
+    return f"setenv bootargs console={console} root={rootfs} rw"
+    # raise NotImplementedError
